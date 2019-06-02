699746a7a866c05ec64e541278b96b4f18342fab
dudwn2329 <dudwn2329@naver.com>
2019-06-02 16:49:52 +0900

    포스팅 완료

--- App/Controller/PostController.php
+++ App/Controller/PostController.php
@@ -2,8 +2,9 @@
 
 namespace Gondr\Controller;
 
-use Gondr\db;
-class PostController extends MasterController{
+use Gondr\DB;
+
+class Postcontroller extends MasterController {
     public function writePage(){
         $this->render("post/write");
     }
@@ -12,14 +13,18 @@ class PostController extends MasterController{
         $title = $_POST['title'];
         $content = $_POST['content'];
         $writer = $_SESSION['user']->id;
-        $sql = "INSERT INTO boards(`title`,`content`,`writer`,`wdate`) VALUES (?, ?, ?, now())";
+
+        $sql= "INSERT INTO boards 
+                (`title`, `content`, `writer`, `wdate`)
+                VALUES (?, ?, ?, NOW())";
         $cnt = DB::query($sql, [$title, $content, $writer]);
 
         if($cnt != 1){
-            $_SESSION['flast_msg'] = ['msg'=>'글 작성중 오류 발생'];
-            hedaer("Location: /post");
+            $_SESSION['flash_msg'] = ['msg'=>'글 작성중 오류 발생'];
+            header("Location: /post");
         }else{
-            $_SESSION['flast_msg'] = ['msg'=>'작성완료'];
+            $_SESSION['flash_msg'] = ['msg'=>'글이 작성되었습니다.'];
+            header("Location: /");
         }
     }
 }
